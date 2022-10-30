4bd3600a4e918fdc60e8c3f3b7ad832deb918729
fanta-git <fantaji4957t@gmail.com>
2022-10-30 15:47:50 +0900

    jsonの読み方をkey基準に変更

--- gas/main.ts
+++ gas/main.ts
@@ -1,3 +1,6 @@
+const ROWS = ['id', 'video_id', 'title', 'artist_id', 'artist_name', 'start_time', 'msec_duration', 'published_at', 'request_user_ids', 'created_at', 'updated_at', 'reasons', 'thumbnail', 'new_fav_user_ids', 'baseinfo', 'colors', 'presenter_user_ids', 'belt_message', 'now_message', 'rotate_action', 'bpm', 'display_playlist_link'] as const;
+const STRING_FORMAT_ROWS = ['request_user_ids', 'new_fav_user_ids', 'presenter_user_ids'] as const;
+
 function test() {
     const response = UrlFetchApp.fetch("https://cafeapi.kiite.jp/api/cafe/timetable?limit=100");
     if (response.getResponseCode() !== 200) throw Error(response.getResponseCode() + " Error");
@@ -12,13 +15,18 @@ function test() {
     const timetableDiff = json.filter(v => v.id > lastId).reverse();
     if (timetableDiff.length === 0) return;
 
-    const writeData = timetableDiff.map(v => Object.entries(v).map(([k, v]) => convertString[k](v)));
-    sheet.getRange(lastRow + 1, 9, writeData.length, 1).setNumberFormat('@');
-    sheet.getRange(lastRow + 1, 14, writeData.length, 1).setNumberFormat('@');
+    const writeData = timetableDiff.map(v => ROWS.map(k =>
+        convertString[k](v[k])
+    ));
+
+    for (const key of STRING_FORMAT_ROWS) {
+        sheet.getRange(lastRow + 1, ROWS.indexOf(key), writeData.length, 1).setNumberFormat('@');
+    }
+
     sheet.getRange(lastRow + 1, 1, writeData.length, writeData[0].length).setValues(writeData);
 }
 
-const convertString: Record<string, (v: any) => string> = {
+const convertString: Record<typeof ROWS[number], (v: any) => string> = {
     id: v => String(v),
     video_id: v => String(v),
     title: v => String(v),
